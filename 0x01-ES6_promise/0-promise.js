91637ff72be7c0fbee27427b561785cac2d908ea
Wessal Ayman <wessalayman237@gmail.com>
2024-10-02 16:23:39 +0300

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,10 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    resolve();
-    reject();
+    const success = true;
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
